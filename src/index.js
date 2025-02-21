f40965fbda45393800e06fa34e4e3ef5f9371fdd
PRAKHAR MADHARIA <155750280+prakhar1304@users.noreply.github.com>
2025-02-21 23:09:55 +0530

    working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("setup");
+
+import connectDB from "./db/index.js";
+
+import dotenv from "dotenv"
+//dotenv.config(); // No need to specify path if your .env is in the root directory
+dotenv.config({path: "./.env"});
+
+
+//when  a  async  process is ende  it  return a promoise
+connectDB()
